800cb8e2d13844dacfc7f34f6f5e8c646f4cdfc0
mriwank <mrizwank@hotmail.com>
2022-03-12 00:30:31 +0500

    update ex15, 16, 17, 18, 19, 20

--- ruby/ex15/ex15.rb
+++ ruby/ex15/ex15.rb
@@ -1,12 +1,28 @@
+#filename = ARGV.first
+
+#txt = open(ex15_sample)
+
+#puts "Here's your file #{ex15_sample}:"
+#print txt.read
+
+#print "Type the filename again: "
+#file_again = $stdin.gets.chomp
+
+#txt_again = open(file_again)
+
+#print txt_again.read
+
+#-----------------------------------------
+
 filename = ARGV.first
 
-txt = open(ex15_sample.txt)
+txt = open(filename)
 
-puts "Here's your file #{ex15_sample.txt}:"
+puts "Here's your file #{filename}:"
 print txt.read
 
 print "Type the filename again: "
-fiel_again = $stdin.gets.chomp
+file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
 
